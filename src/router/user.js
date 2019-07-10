58653b4a8a5d8f39d4fbe3e28e23674653a77f20
chuckiechou <zzp208@qq.com>
2019-07-10 23:58:14 +0800

    login test

--- src/router/user.js
+++ src/router/user.js
@@ -8,6 +8,9 @@ const handleUserRouter = (req, res) => {
         const result = login(username, password);
         return result.then(data => {
             if (data.username) {
+                //操作cookie
+                res.setHeader('Set-Cookie', `username=${data.username}; path=/`);
+
                 return new SuccessModel();
             }
             return new ErrorModel('登录失败');
@@ -16,7 +19,9 @@ const handleUserRouter = (req, res) => {
     //登录验证的测试
     if (method == 'GET' && req.path === '/api/user/login-test') {
         if (req.cookie.username) {
-            return Promise.resolve(new SuccessModel());
+            return Promise.resolve(new SuccessModel({
+                username: req.cookie.username
+            }));
         }
 
         return Promise.resolve(new ErrorModel('尚未登录'))
