ad39d4b2947a6032ee82be652a20b0f8c1063f43
chuckiechou <zzp208@qq.com>
2019-07-12 07:52:44 +0800

    add redis lib

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,5 +1,12 @@
 const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
+
+const loginCheck = (req) => {
+    if (!req.session.username) {
+        return new Promise(new ErrorModel('尚未登录'));
+    }
+}
+
 const handleBlogRouter = (req, res) => {
     const method = req.method;
     const id = req.query.id;
@@ -21,7 +28,13 @@ const handleBlogRouter = (req, res) => {
     }
 
     if (method === 'POST' && req.path === '/api/blog/new') {
-        req.body.author = 'zhangsan';
+
+        const loginCheckResult = loginCheck(req);
+        if (loginCheckResult) {
+            return loginCheckResult;
+        }
+
+        req.body.author = req.session.username;
         const result = newBlog(req.body);
         return result.then(data => {
             console.log(data);
@@ -30,6 +43,12 @@ const handleBlogRouter = (req, res) => {
     }
 
     if (method === 'POST' && req.path === '/api/blog/update') {
+
+        const loginCheckResult = loginCheck(req);
+        if (loginCheckResult) {
+            return loginCheckResult;
+        }
+
         const result = updateBlog(id, req.body);
         return result.then(val => {
             if (val) {
@@ -41,7 +60,13 @@ const handleBlogRouter = (req, res) => {
     }
 
     if (method === 'POST' && req.path === '/api/blog/del') {
-        const author = 'zhangsan';
+
+        const loginCheckResult = loginCheck(req);
+        if (loginCheckResult) {
+            return loginCheckResult;
+        }
+
+        const author = req.session.username;
         const result = delBlog(id, author);
         return result.then(val => {
             if (val) {
--- src/router/user.js
+++ src/router/user.js
@@ -10,8 +10,8 @@ const getCookieExpire = () => {
 
 const handleUserRouter = (req, res) => {
     const method = req.method;
-    if (method === 'GET' && req.path === '/api/user/login') {
-        const { username, password } = req.query;
+    if (method === 'POST' && req.path === '/api/user/login') {
+        const { username, password } = req.body;
         const result = login(username, password);
         return result.then(data => {
             if (data.username) {
